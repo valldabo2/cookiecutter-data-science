8881eac28eb98051162409db9a7c5b8c67a0f0af
Dahlen <olle.dahlen@ing.com>
2019-06-27 15:03:15 +0200

    Tries to make a conditional file

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+
+create_template_project = '{{cookiecutter.create_template_project}}'
+
+if not create_template_project:
+    # remove top-level file inside the generated folder
+    remove(os.path.join(os.getcwd(), '{{cookiecutter.package_name}}', 'test.py'))
+
