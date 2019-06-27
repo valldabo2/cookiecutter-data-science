1ce358bd6661b97435097e9cf50b6aa4e0f7b923
Dahlen <olle.dahlen@ing.com>
2019-06-27 16:42:23 +0200

    Updated post hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,6 +11,10 @@ def remove(filepath):
 
 create_template_project = '{{cookiecutter.create_template_project}}'
 
+
+for i in range(10):
+    print(create_template_project)
+
 if not create_template_project:
     # remove top-level file inside the generated folder
     remove(os.path.join(os.getcwd(), '{{cookiecutter.project_name}}', 'test.py'))
